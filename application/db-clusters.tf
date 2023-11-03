82ce36a7d14ab6ce136dd697b68b1ff9dc9ce8d2
ian-hancock <46765905+ian-hancock@users.noreply.github.com>
2023-11-03 15:32:39 +1000

    Update db-clusters.tf

--- application/db-clusters.tf
+++ application/db-clusters.tf
@@ -1,12 +1,12 @@
 module "marketing_cluster" {
-    source       = "./modules/cluster"
+    source       = "../modules/cluster"
     cluster_name = "${var.env}-marketing-cluster"
     usergroups   = ["MarketingDataUsers"]
     managegroups = ["MarketingDataStewards"]
 }
 
 module "finance_cluster" {
-    source       = "./modules/cluster"
+    source       = "../modules/cluster"
     cluster_name = "${var.env}-finance-cluster"
     usergroups   = ["FinanceDataUsers"]
     managegroups = ["FinanceDataStewards"]
