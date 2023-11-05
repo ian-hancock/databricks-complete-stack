1002f981ff70bce65ff7766aab50f873dd5907b8
ian-hancock <46765905+ian-hancock@users.noreply.github.com>
2023-11-06 09:42:41 +1000

    Update req version

---
+++ modules/cluster/provider.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_version = "~> 1"
+  required_providers {
+    databricks = {
+      source = "databricks/databricks"
+      version = "1.27.0"
+    }
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.19.0"
+    }
+  }
+}
